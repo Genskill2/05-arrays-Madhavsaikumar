b7a974f551c101159fe38b9a1b670b415a567b3e
Madhavsaikumar <84717045+Madhavsaikumar@users.noreply.github.com>
2021-06-15 12:07:05 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,13 +1,11 @@
 /* Enter your solutions in this file */
 #include<stdio.h>
 int n;int i;
-int max(int a[n],n);
+int max(int a[],int );
 int main(void){
-int a[n];int max；
-n = get_int("enter the total elements of array");
-for(i=0;i<n;i++){
-a[i] = get_int("enter the %i number",i+1);
-}
+int a[n];int max;
+n = number of elements of array;
+a[n] = {10,20,30,40,50}
 max = a[0];
 for(i=1;i<n;i++){
 if(a[i]>max){
