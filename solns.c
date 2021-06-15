53e474f1db85cc6a10ff42da41069c5cd3d93d06
Madhavsaikumar <84717045+Madhavsaikumar@users.noreply.github.com>
2021-06-15 12:16:19 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,11 +1,20 @@
 /* Enter your solutions in this file */
 #include<stdio.h>
+#include <assert.h>
+int max(int [], int);
+int main(void) {
+  int x[] = {9,5,6,10,2,-3,4};
+  assert (max(x, 7) == 10);
+
+  int y[] = {5};
+  assert (max(y, 1) == 5);
+  printf("Max: passed\n");
+  }
 int n;int i;
-int max(int a[],int );
+int max(int a[n],int n);
 int main(void){
-int a[n];int max;
-n = number of elements of array;
-a[n] = {10,20,30,40,50}
+int a[n];int max；
+int max(int [], int);
 max = a[0];
 for(i=1;i<n;i++){
 if(a[i]>max){
