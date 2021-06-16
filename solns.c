9083d1c5972d12823979dda8b0373fb83e877c86
Madhavsaikumar <84717045+Madhavsaikumar@users.noreply.github.com>
2021-06-16 11:13:25 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,29 +1,19 @@
 /* Enter your solutions in this file */
 #include<stdio.h>
-#include <assert.h>
-int max(int [], int);
-int main(void) {
-  int x[] = {9,5,6,10,2,-3,4};
-  assert (max(x, 7) == 10);
-
-  int y[] = {5};
-  assert (max(y, 1) == 5);
-  printf("Max: passed\n");
-  }
-int n;int i;
-int max(int a[n],int n);
-int main(void){
-int a[n];int max；
-int max(int [], int);
-n = get_int("enter the total elements of array");
-for(i=0;i<n;i++){
-a[i] = get_int("enter the %i number",i+1);
+int min(int[ ],int);
+int main(void)
+{
+int x[ ]={5};
+printf("%i",min(x,1));
 }
-max = a[0];
-for(i=1;i<n;i++){
-if(a[i]>max){
-max = a[i];
-}
-}
-printf("maximum of the given numbers in array is %d\n”,max);
+int min(int a[ ],int n)
+{
+    int mi=a[0];
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]<mi){
+            mi=a[i];
+    }
+    }
+return mi;
 }
