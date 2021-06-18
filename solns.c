d9cff69c3d4b6936407677a1b66a83341bc31ca3
Madhavsaikumar <84717045+Madhavsaikumar@users.noreply.github.com>
2021-06-18 23:10:31 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,23 +1,84 @@
 /* Enter your solutions in this file */
 #include<stdio.h>
-int max(int a[ ], int n)
-{
-    int i;
-    
-    int max = arr[0];
- 
-    for (i = 1; i < n; i++){
-        if (arr[i] > max){
-            max = arr[i];
- }
- }
- return max;
+
+/*average function*/
+float average(int arr[], int len){
+  int sum = 0;
+  float avg;
+  for(int p=0; p<len; p++){
+    sum = sum + arr[p];
+  }
+  avg = (float)sum/len;
+  return avg;
 }
- 
-int main(void)
-{
-    int a[ ] = {10, 324, 45, 90, 9808};
-    int n = sizeof(a)/sizeof(a[0]);
-    printf("Maximum integer in given array is %d", max(a, n));
-    return 0;
+/*factors function*/
+int factors(int num, int arr[]){
+int cpy = num;
+int cnt = 0;
+int cnt1 =0;
+for(int i = 2; i<num/2; i++){
+if(num%i ==0){
+for(int j=2; j<=i; j++){
+if(i%j==0)
+cnt+= 1;
 }
+if(cnt ==1){
+while(cpy%i ==0){
+arr[cnt1] = i;
+cpy/= i;
+cnt1+= 1;
+}
+cpy = num;}
+cnt = 0;
+}
+else
+continue;}
+return cnt1;
+}
+/* max function*/
+int max(int arr[], int len){
+  int maximum = arr[0];
+  for(int i= 0; i< len; i++){
+    if(arr[i]> maximum)
+      maximum = arr[i];
+  }
+  return maximum;
+       }
+/* min function*/
+     int min( int arr[], int len){
+  int min;
+  min = arr[0];
+  for(int h=0; h<len; h++){
+    if(arr[h]<min){
+      min = arr[h];
+    }
+  }
+  return min;
+}
+/* mode function*/
+int mode(int arr[], int len){
+  int ar2[len];
+  int n,p, max;
+  int cnt = 0;
+  for(int i = 0; i< len; i++){
+  n = arr[i];
+  for(int j=0; j<len; j++){
+  if( arr[j] == n)
+  cnt = cnt +1;
+  }
+  ar2[i] = cnt;
+  cnt = 0;
+  }
+  max = ar2[0];
+  for(int h = 0; h< len; h++){
+  if(ar2[h]> max){
+  max = ar2[h];
+  }
+  }
+  for(p =0; p<len; p++){
+  if(max == ar2[p])
+  break;
+  }
+  return arr[p];
+  }
+
